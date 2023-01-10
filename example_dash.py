078b23041e38db63787afd0010edd49a9c6cf362
dylan_jacquot <Dylan.Jacquot.Etu@univ-lemans.fr>
2023-01-10 19:02:32 +0100

    V0.50 Update coefficients

--- example_dash.py
+++ example_dash.py
@@ -7,13 +7,34 @@ import math
 from functions import initializeStudent
 from student import Student
 
-def create_slider(label):
+our_hypothesis = {"Ingénieur expert technique":-1,"Ingénieur, homme de culture":1,"Ingénieur chef de projet":0,
+              "Procédural":-1,"Orienté objet":1,"Fonctionnel":1,"Web":1,"Cloud":0,"Embarqué":-1,"Mobile":1,
+              "Développement de jeux vidéos":1,"Mathématiques":-1,"Physique-Chimie":-1,
+              "Littérature et Philosophie":1,"Histoire Géographie":1,"Gestion":0,"Arts appliqués":1,"Sciences de l'ingenieur":-1,
+              "S5 cryptographie":-1,"S5 physique":-1,"S5 anglais":1,"S5 conduite de projet":0,"S5 droit du travail":0,
+              "S5 Électronique":-1,"S5 Programmation":0,"S5 Algorithmique":0,"S5 Technologie de l'internet":1,
+              "S5 Architecture des ordinateurs":-1,"S5 Logique combinatoire et séquentielle":-1,
+              "S5 Introduction aux IHM":1,"LabView":-1, "Excel":1, "Blender":1, "Unity":1, "Figma":1,
+              "Cisco Packet Tracer":-1, "SolidWorks":-1, "AutoCAD":-1,"Imprimante 3D":-1, "Drone":-1, "Casque VR":1,
+              "Carte électronique programmable":-1, "Oscilloscope":-1, "Casque neuronal":0, "Smartphone":0, "Rien":0,
+              "K[art]el":1, "Ensimersion":1, "Ensim'Elec":-1, "Enigma":0, "L'Ensimien":1, "Statue de Mario":1,
+              "Boîtier pour carte Arduino":-1, "Bouton":-1, "Coque de téléphone":0, "Lecture":1, "Bricolage":-1,
+              "Sport":0, "Art et dessin":1, "Jeux vidéos":0, "Musique":0, "Musculation":0,
+              "Utilisation de palettes de couleur":1, "Non utilisation de palettes de couleurs":0, "Démontage du PC":-1
+              , "Réparation par le BIOS":-1, "Consulter un réparateur":1, "Acheter un nouveau PC":1, "Prépa intégrée":0,
+              "CPGE ATS":0, "CPGE PT":-1, "CPGE TSI":-1, "CPGE MP":-1, "CPGE MPSI":-1, "CPGE BL":1, "CPGE PC":-1,
+              "BTS SIO":0, "BTS SN":-1, "DUT Informatique":1, "DUT Métiers du multimédia et de l'internet":1,
+              "L2/L3 Informatique":0, "M1/M2 Informatique":-1, "Windows":1, "MacOS":1, "Linux":-1}
+
+hypothesis_values = list(our_hypothesis.values())
+
+def create_slider(label,base_value):
     html_label = html.Label(str(label)),
     html_slider = dcc.Slider(
                min=-5,
                max=5,
                step=0.2,
-               value=1,
+               value=base_value,
                tooltip={"placement": "bottom", "always_visible": True},
                updatemode='drag',
                persistence=True,
@@ -59,8 +80,9 @@ app = Dash(__name__)
 div_content = [
         html.H1("Choix ASTRE-IPS", style={'textAlign': 'center'}),
     ]
-for label in labels:
-    html_label, html_slider = create_slider(label)
+
+for label,value in our_hypothesis.items():
+    html_label, html_slider = create_slider(label,value)
     div_content.append(html_label[0])
     div_content.append(html_slider[0])
 
